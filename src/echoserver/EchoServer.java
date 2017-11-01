d073bfab48ae0b1207f552a0b0b710ba5e31d5f2
root <root@DESKTOP-FK3SQFB.localdomain>
2017-11-01 12:10:56 -0500

    I belive I got the code in for the server side.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,11 +7,29 @@ public class EchoServer {
 		try{
 			ServerSocket SSocket= new ServerSocket(6013);
 			while(true){
-			//Do something
-			} catch(IOException NoGo){
-				System.err.println(NOGo);
+				int alive;
+				//Need socket from user
+				Socket user = SSocket.accept();
+				
+				//Need more handshaking
+				InputStream inUser = user.getInputStream();
+				OutputStream outUser = user.getOutputStream();
+				
+				//keeping the info alive from the User
+				while((alive = inUser.read())!=-1){
+					inClient.write(alive);
+					//Useing flush to write out any data that remains in the 
+					//stream
+					//, happens to be a common comand for the wiiner of the 
+					//sorting compuitution this year
+					outUser.flush();
+				}
+					
+			}
 			//If not true
-		}
+			catch(IOException NoGo){
+				System.err.println(NOGo);
+			}
 		}
 	}
 }
