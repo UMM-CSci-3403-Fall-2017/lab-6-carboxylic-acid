80cf1e5626b9fe0060e2d6a526e9d138cfb839fb
root <root@DESKTOP-FK3SQFB.localdomain>
2017-11-01 11:59:28 -0500

    working on EchoServer.java added catch and try into the code

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,6 +1,17 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
-public class EchoServer {
-needs stuff here :P
-
+public class EchoClient {
+	public static void main(string[] agruments){
+		try{
+			ServerSocket SSocket= new ServerSocket(6013);
+			while(true){
+			//Do something
+			} catch(IOException NoGo){
+				System.err.println(NOGo);
+			//If not true
+		}
+		}
+	}
 }
