167b890257adc46c99c623a84d051b83530f310c
root <root@DESKTOP-FK3SQFB.localdomain>
2017-11-02 11:54:28 -0500

    added some code and help steps to continue on when comming back, need to push data to sever and get a reply, also need to write in a catch for the try

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,26 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+	public static main(String[] args){
+		try{
+			//Opens a socket
+			Socket Csocket = new Socket("127.0.0.1", 6013);
+			
+			InputStream thisone = new DataInputStream(System.thisone)
+			//Define a stream for the byetes to be handled
+			InputStream Sin = Csocket.getInputStream();
+			OutputStream Sout = Csocket.getOutputStream();
+			// This is a variable that hold data
+			int a;
+			while(check if infos exist){
+				//write
+				//clean stream
+				//Maybe servertime?
+			}
+			//close
+		}
+		//catch
+	}
 }
