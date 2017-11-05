fa0dd93f138b045201ebe300348bd2558a0ec451
root <root@DESKTOP-FK3SQFB.localdomain>
2017-11-05 15:22:15 -0600

    fixed EchoClient, and got it to pass all test in bats

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -3,24 +3,32 @@ import java.net.*;
 import java.io.*;
 
 public class EchoClient {
-	public static main(String[] args){
+	public static void main(String[] args){
 		try{
 			//Opens a socket
 			Socket Csocket = new Socket("127.0.0.1", 6013);
-			
-			InputStream thisone = new DataInputStream(System.thisone)
+			//Orginally had this one name ThisOne but it dosent compille :(
+			//so it was changed to in vs ThisOne
+			InputStream in = new DataInputStream(System.in);
+			OutputStream Sout = Csocket.getOutputStream();
+
 			//Define a stream for the byetes to be handled
 			InputStream Sin = Csocket.getInputStream();
-			OutputStream Sout = Csocket.getOutputStream();
-			// This is a variable that hold data
-			int a;
-			while(check if infos exist){
-				//write
-				//clean stream
-				//Maybe servertime?
+			// This is a variable that holds data have bytes to handle binary stuff
+			byte[] BufferIn = new byte[1];
+			byte[] Bufferout = new byte[1];
+			
+			while(in.read(BufferIn)!=-1){
+				Sout.write(BufferIn);
+				Sout.flush();
+				Sin.read(Bufferout);
+				System.out.write(Bufferout,0,1);
+				
 			}
-			//close
+			Csocket.close();
 		}
-		//catch
+		catch(IOException NoGo){
+			System.err.println(NoGo);
+		}
 	}
 }
